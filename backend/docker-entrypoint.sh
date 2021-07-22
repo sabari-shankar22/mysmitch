fd45536cf7ea88c57e185c533f8b05ae80c242cb
sabari <sabarishankar22@gmail.com>
2021-07-22 20:31:01 +0530

    Adding Redis
    Adding configuration for redis container in docker-compose,

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,5 +1,8 @@
 echo "Waiting for MongoDB to start..."
 ./wait-for db:27017 
 
+echo "Waiting for Redis to start..."
+./wait-for redis:6379
+
 echo "Starting the server..."
 npm start
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const reddis = require('redis');
+
+const redisClient = reddis.createClient(6379,'redis');
 const app = express();
 
-mongoose.connect('mongodb://db/vidly')
+mongoose.connect('mongodb://db:27017/user')
     .then(() => console.log('Connected to MongoDB...'));
+
+redisClient.on('connect', function() {
+  console.log('Connected to Redis!');
+});
+redisClient.on("error", function(error) {
+  console.log(error);
+});
 
 app.get("/", (req, res) => {
     res.send("<h1>Welcome to User Services!</h1>");
